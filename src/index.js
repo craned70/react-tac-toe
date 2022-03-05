52526cc99095893388639ec9ea8f18f7faad6994
craned70 <craned70@gmail.com>
2022-03-05 14:22:15 -0800

    Save progress in tutorial
    went over passing data through props, making a component interactive, and lifting state up

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
-
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -40,6 +41,19 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button 
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
     );
   }
 }
