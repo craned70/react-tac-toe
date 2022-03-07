a8d229cbae081fd05f9165e32f351a5e39e53472
craned70 <craned70@gmail.com>
2022-03-06 23:32:06 -0800

    refactor comments
    make the comment inline instead of newline so the code itself is actually readable

--- src/index.js
+++ src/index.js
@@ -1,59 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Board } from "./board.js";
+import { calculateWinner } from "./squares.js"
 import './index.css';
 
 class Game extends React.Component {
-    //boilerplate class component syntax
-    constructor(props) {
+    constructor(props) { //boilerplate class component syntax
       super(props);
-      // set state to include history -- an array of objects, each object contains an array of the board state at a certain point in the game
-      this.state = {
+      this.state = { // set state to include history -- an array of objects, each object contains an array of the board state at a certain point in the game
         history: [{
           squares: Array(9).fill(null),
         }],
-        // set state to determine who's turn it is
-        xIsNext: true,
+        xIsNext: true, // set state to determine who's turn it is
       };
     }
   
-    // handleClick function - this is called when a square is clicked
     handleClick(i) {
-      // bind history to a var
       const history = this.state.history;
-      // define the current board state
       const current = history[history.length - 1];
-      // makes a copy of the board
-      const currentSquares = current.squares.slice();
-      // check if there's a winner, or the square clicked is already filled, if either is true, return early
-      if (calculateWinner(currentSquares) || currentSquares[i]) {
+      const currentSquares = current.squares.slice(); // this makes a copy of the board, cuz mutatin' data be bad
+      if (calculateWinner(currentSquares) || currentSquares[i]) { // check if there's a winner, or the square clicked is already filled, if either is true, return early
         return;
       }
-      // fill the square that is clicked with X or O depending on who's turn it is
-      currentSquares[i] = this.state.xIsNext ? 'X' : 'O';
-      // store this board in history (in the tutorial they write in setState - squares: squares, which is really confusing IMO so I renamed the variable to currentSquares)
-      this.setState({
-        history: history.concat([{
-          squares: currentSquares,
+      currentSquares[i] = this.state.xIsNext ? 'X' : 'O'; // fill the square that is clicked with X or O depending on who's turn it is
+      this.setState({ 
+        history: history.concat([{ // store this board in history
+          squares: currentSquares, // note: in the tutorial the code here is squares: squares, which is really confusing IMO so I renamed the variable to currentSquares
         }]),
-        // flip to the next person's turn
-        xIsNext: !this.state.xIsNext,
+        xIsNext: !this.state.xIsNext, // flip to the next person's turn
       });
     }
   
-    // render describes the view to be rendered in the browser window
+
     render() {
-      // bind history to a var
       const history = this.state.history;
-      // define the current board state
       const current = history[history.length - 1];
-      // check for a winner
       const winner = calculateWinner(current.squares);
   
-      // mapping the history array of past boards - step is the element value and move refers to the indice
+      // mapping the history array of past boards - "step" is the value of each element and "move" refers to the indice
       const moves = history.map((step, move) => {
-        // ternary to check if move (the indice) is truthy, if not that means we have no board history (0), so desc will say to go to the board start, if it is then it will say to go to that move in the board's history.
-        const desc = move ?
+        const desc = move ? // ternary, if we're at the 0 indice that means we have no board history thus the button being rendered should be to go to the start, if move is > 0 is then it will say to go to that move in the board's history.
           'Go to move #' + move :
           'Go to game start';
         return (
@@ -63,10 +49,8 @@ class Game extends React.Component {
         );
       })
   
-      // declare a status var
       let status;
-      // check if there is a winner and then display who it is, if true. otherwise display who's turn is next
-      if (winner) {
+      if (winner) { // check if there is a winner and then display who it is, if true. otherwise display who's turn is next
         status = 'Winner: ' + winner;
       } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -76,10 +60,8 @@ class Game extends React.Component {
         <div className="game">
           <div className="game-board">
             <Board 
-              //display current board state, we're passing squares as props to the board child component
-              squares={current.squares}
-              // onClick call the handleClick function, we're passing this function as props to the board child component
-              onClick={(i) => this.handleClick(i)}
+              squares={current.squares} //display current board state, we're passing squares as props to the "Board" child component
+              onClick={(i) => this.handleClick(i)} // onClick call the handleClick function, we're passing this function as props to the "Board" child component
             />
           </div>
           <div className="game-info">
@@ -91,29 +73,7 @@ class Game extends React.Component {
     }
   }
 
-function calculateWinner(squares) {
-  // list of all winning lines - an X or O existing in all 3 of any of these indices indicates a win
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3 ,6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  // loop through each winning line, checking if there is an X or an O in every location, return which one is the winner, if no winner return null
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-  //----------------------------------------------
+  //==========================================
 
   ReactDOM.render(
     <Game />,
--- src/squares.js
+++ src/squares.js
@@ -8,3 +8,25 @@ export function Square(props) {
       </button>
     );
   }
+
+function calculateWinner(squares) {
+  // list of all winning lines - an X or O existing in all 3 of any of these indices indicates a win
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3 ,6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // loop through each winning line, checking if there is an X or an O in every location, return which one is the winner, if no winner return null
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
